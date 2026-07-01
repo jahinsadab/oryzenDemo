a79045f797dbd495ab52a09b6829144ae26dccb8
jahinsadab <jahinsadab98@gmail.com>
2026-07-02 02:36:36 +0600

    13

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+// MENU MODAL POPUP
+// ====================================
+
+document.addEventListener("DOMContentLoaded", function () {
+  const modal = new bootstrap.Modal(document.getElementById("exampleModal"));
+
+  modal.show();
+});
+
+function closeImage() {
+  const modalEl = document.getElementById("exampleModal");
+  const modal = bootstrap.Modal.getInstance(modalEl);
+
+  if (modal) {
+    modal.hide();
+  }
+}
+
 //  MOBILE HEADER
+// ====================================
 
 document
   .querySelector(".bi-search")
